a1e6885474c5c587d6c9ac7c95b5c3eca8c184e6
Gustavo Restrepo <grestrepo@ingecall.com>
2021-11-08 10:05:21 -0500

    App terminada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  pokemon_url: 'https://pokeapi.co/api/v2/pokemon',
+  pokemon_url: 'https://pokeapi.co/api/v2',
   pokemon_api: 'https://prueba-pokemon-api.herokuapp.com/api/v1/pokemons/battle'
 };
